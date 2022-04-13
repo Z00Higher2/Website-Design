c3d8a0e91d41d21d886641d704faf46de64f2010
Z00Higher <90710904+Z00Higher@users.noreply.github.com>
2022-04-13 11:31:07 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
